3b99bfe763a8349815d11fd487ec00a09373fda4
arensburger <arensburger@gmail.com>
2014-11-08 21:21:39 -0800

    upadated report-tir-ele-overlap.pl

--- report-tir-ele-overlap.pl
+++ report-tir-ele-overlap.pl
@@ -2,34 +2,36 @@
 
 use strict;
 use Getopt::Long;
+use File::Temp();
 
 my $SIDE_SEQUENCE_LENGTH = 20; #number of bp to to get on each side of the element
-#my $TSD_LENGTH = 2;
-#my $TIR_LENGTH = 11;
 
 my %config;
 GetOptions(\%config,
 	'rmbed=s',
 	'tirbed=s',
 	'genome=s',
+	'dbte=s',
 	'out=s',
 );
 
-##> Check if no mandatory parameter is missing and set defaults
+##> Check if no mandatory parameter is missing, set defaults, and output file
 if (!exists $config{rmbed})         {printUsage();}
 if (!exists $config{tirbed})         {printUsage();}
 if (!exists $config{genome})         {printUsage();}
+if (!exists $config{dbte})         {$config{dbte} = "./TEdiscovery/repbase_classII.fa";}
 if (!exists $config{out}) {$config{out} = "out";}
+open (OUTPUT, ">$config{out}") or die "cannot open output file $config{out}\n";
 
 ## Main program ##
+
 my %species; # holds the name of the element as key and an array with each instance detail as value 
+
 ### find overlaps between the databases using bedtools ###
-#my $overlapfile = `bedtools intersect -a $config{tirbed} -b $config{rmbed} -wo`;
 my @lines = split /\n/, `bedtools intersect -a $config{tirbed} -b $config{rmbed}  -wo`; 
 unless (@lines) { # checking that something got put into the array
 	die "ERROR, bedtools intersection did not work, check that bedtools is installed\n";
 }
-
 
 ### read through the file ###
 my %seen; #to prevent duplications
@@ -43,7 +45,8 @@ foreach my $line (@lines) {
 	my $type = $data[7];
 	my $ori = $data[9];
 	my $overlap = $data[10];
-	#determine the TSD and TIR length for current element
+
+	# determine the TSD and TIR length for current element
 	my $tsd_length;
 	my $tir_length;
 	if($data[3] =~ /(\S+)_(\S+)/) {
@@ -69,10 +72,10 @@ foreach my $line (@lines) {
 	
 	}	
 
+	# if the tir and predicted te sequences overlap (i.e. the te is inside the tirs) then record this event
 	if (($eleb2 - $eleb1) == $overlap) { #true if the sequences overlap fully
 		my @data2 = split ("#", $type);
-#		print "$data2[0], $data2[1], $data2[2], $type\n";
-#		print "$scaffold, $tirb1, $tirb2, $eleb1, $eleb2, $type\n";
+
 		# add line into %species
 		my $seen_summary =  "$scaffold\t$tirb1\t$tirb2\t$ori\t$data[7]";
 		unless (exists $seen{$seen_summary}) {
@@ -83,18 +86,20 @@ foreach my $line (@lines) {
 }
 
 ### report data on each element ###
+
 my %genome = genometohash($config{genome});
-for my $element ( keys %species ) {
-	my @element_data; # holds all the elements for this species
+
+for my $element (keys %species ) { # scroll throught elements
+	my @element_data; # holds all the elements for this species of TE
 #print "$element:\n";
 	for my $i ( 0 .. $#{ $species{$element} } ) {
 		my @data = split("\t", $species{$element}[$i]);
 
-		#get the tir and tsd lengths
+		## get the tir and tsd lengths
 		my $tsd_length = $data[8];
 		my $tir_length = $data[9];
 
-		#get the element sequence
+		## get the element sequence
 		my $element_sequence;
 		if ($data[5] eq "+") {
 			$element_sequence = substr($genome{$data[$data[0]]}, ($data[1] - $SIDE_SEQUENCE_LENGTH), ($data[2] - $data[1] + $SIDE_SEQUENCE_LENGTH + $SIDE_SEQUENCE_LENGTH)); 
@@ -106,6 +111,7 @@ for my $element ( keys %species ) {
 			die "unexpected orientation $data[5]\n";
 		}
 	
+		## collect the rest of the information about this element 
 		my $left_side_sequence = substr($element_sequence, 0, $SIDE_SEQUENCE_LENGTH);
 		my $left_TSD = substr($element_sequence, $SIDE_SEQUENCE_LENGTH, $tsd_length);
 		my $left_TIR = substr($element_sequence, ($SIDE_SEQUENCE_LENGTH + $tsd_length), $tir_length);
@@ -114,11 +120,9 @@ for my $element ( keys %species ) {
 		my $right_TIR = substr($element_sequence, -($SIDE_SEQUENCE_LENGTH + $tsd_length + $tir_length), $tir_length); 
 		my $te_sequence = substr($element_sequence, ($SIDE_SEQUENCE_LENGTH + $tsd_length), (length $element_sequence) - $SIDE_SEQUENCE_LENGTH - $SIDE_SEQUENCE_LENGTH - $tsd_length - $tsd_length); 
 
-#print "$element_sequence\n";
-#print "$left_side_sequence\t$left_TIR\t$right_TIR\t$right_side_sequence\t$data[0]\t$data[1]\t$data[2]\n";
-#print "$left_TSD, $right_TSD\n";
-#print "$te_sequence\n";
-		# store this element, the test prevents adding elements that could not be properly determined
+		## store the information into the @element_data array
+
+		# this test prevents adding element data that could not be properly determined for one reason or another
 		unless ((length $left_side_sequence == 0) or (length $left_TSD == 0) or (length $left_TIR == 0)) {
 			my $location = $data[0] . ":" . $data[1] . "-" . $data[2];
 			chomp $data[7];
@@ -127,33 +131,41 @@ for my $element ( keys %species ) {
 		}
 	}
 
-	#compare all the elements for this species to each other
+	## compare all the elements for this TE species to each other
+
 	my %insert_data; #holds all the information about insertion data for each element
 	my %sequence_data; #holds the sequence for each element
 
-	for (my $i=0; $i< ((scalar @element_data) - 1); $i++) {
-		my @data = split("\t", $element_data[$i]);
-		my $location1 = $data[4];
-		my $tirs1 = $data[1] . $data[2];
-		my $side_sequences1 = $data[0] . $data[3];
-		my $id1 = $data[5];
-		my $tsd1 = $data[7];
-		my $te_sequence1 = ">" . $data[4] . "#" . $id1 . "#" . $element . "\n" . $data[6];
-		for (my $j=$i+1; $j<scalar @element_data; $j++) {
-			my @data2 = split("\t", $element_data[$j]);
-			my $location2 = $data2[4];
-			my $tirs2 = $data2[1] . $data2[2];
-			my $side_sequences2 = $data2[0] . $data2[3];
-			my $id2 = $data2[5];
-			my $tsd2 = $data2[8];
-			my $te_sequence2 = ">" . $data2[4] . "#" . $id2 . "#" . $element . "\n" . $data2[6];
+	for (my $i=0; $i< ((scalar @element_data) - 1); $i++) { ## first loop 
 
-			# testing for uniqueness
+		my @data = split("\t", $element_data[$i]);
+		my $location1 = $data[4]; # genomic location of first member of pair
+		my $tirs1 = $data[1] . $data[2]; # combined TIR sequences of first member of pair
+		my $side_sequences1 = $data[0] . $data[3]; # combined side sequences of first member of pair
+		my $id1 = $data[5]; # unique id of first member of pair
+		my $tsd1 = $data[7]; # TSD sequence of first member of pair
+		my $te_sequence1 = ">" . $data[4] . "#" . $id1 . "#" . $element . "\n" . $data[6]; # fasta formated genomic sequence of first member of pair
+
+		for (my $j=$i+1; $j<scalar @element_data; $j++) { ## second loop
+
+			my @data2 = split("\t", $element_data[$j]); 
+			my $location2 = $data2[4]; # genomic location of second member of pair
+			my $tirs2 = $data2[1] . $data2[2]; # combined TIR sequences of second member of pair
+			my $side_sequences2 = $data2[0] . $data2[3]; # combined side sequences of second member of pair
+			my $id2 = $data2[5]; # unique id of second member of pair
+			my $tsd2 = $data2[8]; # TSD sequence of second member of pair
+			my $te_sequence2 = ">" . $data2[4] . "#" . $id2 . "#" . $element . "\n" . $data2[6]; # fasta formated genomic sequence of second member of pair
+
+			## if the two members of the pair are similar enough to be called the same
+
 			my $TIR_IDENTITY_THRESHOLD = 0.1; # maximum p distance to call two tirs identical
 			my $SIDE_IDENTITY_THRESHOLD = 0.7; # minimum distance between the side sequences to call them different 
-			if ($location1 ne $location2) {
+
+			if ($location1 ne $location2) { # only continue if the two sequences are not just reverse complements of each other
+
 				my $passed_tir_test = 0; # boolean, 0 if tirs are identical enough 1 if not
 				my $passed_side_test = 0; # boolean, 0 if side sequence are the same 1 if they are different
+
 				if (pdistance ($tirs1, $tirs2) < $TIR_IDENTITY_THRESHOLD) {
 					$passed_tir_test = 1;
 				}
@@ -162,38 +174,97 @@ for my $element ( keys %species ) {
 				}
 
 				if ($passed_tir_test and $passed_side_test) {
-					my $insertion_data1 = "$data[0]\t$data[7]\t$data[1]\t$data[2]\t$data[8]\t$data[3]\t$location1";
-					my $insertion_data2 = "$data2[0]\t$data2[7]\t$data2[1]\t$data2[2]\t$data2[8]\t$data2[3]\t$location2";
+					my $insertion_data1 = "$data[0]\t$data[7]\t$data[1]\t$data[2]\t$data[8]\t$data[3]\t$location1]\t$te_sequence1";
+					my $insertion_data2 = "$data2[0]\t$data2[7]\t$data2[1]\t$data2[2]\t$data2[8]\t$data2[3]\t$location2\t$te_sequence2";
 					$insert_data{$insertion_data1} = "$data[1]\t$data[2]";
 					$insert_data{$insertion_data2} = "$data2[1]\t$data2[2]";
-					$sequence_data{$te_sequence1} = 0;
-					$sequence_data{$te_sequence2} = 0;
 				}
 			}			
 		}
 	}
 
-	# print if any results found
+	## Output the data if any
 	if (%insert_data) {
-		print "********\n";
+		print OUTPUT "********\n";
 		foreach my $key (sort {$insert_data{$a} cmp $insert_data{$b} } keys %insert_data) {
-			print "$key\n";
+			my @data = split("\t", $key);
+			my ($hitname, $evalue, $overlap) = blastseq($data[7]);
+			my @data2 = split("\n", $data[7]);
+			print OUTPUT "$data[0]\t$data[1]\t$data[2]\t$data[3]\t$data[4]\t$data[5]\t$hitname\t$evalue\t$overlap\t$data2[0]\t$data2[1]\n"; #all but location
+#			print OUTPUT "$key\n";
 		}
-		foreach my $key (keys %sequence_data) {
-			print "$key\n";
+#		foreach my $key (keys %sequence_data) {
+#			print "$key\n";
+#		}
+#		print "\n";
+	}
+}
+
+# takes a sequence as input, does a blast search against a known db of TE proteins returns data about the hit 
+sub blastseq {
+	my ($sequence) = @_;
+
+	# setup file for blast
+	my $sequence_file = File::Temp->new( UNLINK => 1, SUFFIX => '.fa' ); # current sequence file
+	my $blast_file = File::Temp->new( UNLINK => 1, SUFFIX => '.blast' ); # current sequence file
+	open (SEQ, ">$sequence_file") or die "cannot open temporary file $sequence_file\n";
+	print SEQ "$sequence\n";
+	close SEQ;
+
+	# execute the blast
+#`cp $sequence_file temp`;
+#print "$sequence_file\n"; exit;
+	`./fastx36 -E 1 $sequence_file $config{dbte} > $blast_file`;
+#	unless ($blast_file) {
+#		die "blast did not work, check that fastx36 is installed\n";
+#	}
+`cp $blast_file temp`; 
+	# interpret the blast results
+	my $tename;
+	my $te_length;
+	my $evalue;
+	my $overlap;
+
+	if ($blast_file) {
+		open (BLAST, $blast_file) or die "cannot open file $blast_file\n";
+		while (my $line = <BLAST>) {
+			if ($line =~ /^>>(\S+)\s+\((\d+)\saa\)/) {
+				$tename = $1;
+				$te_length = $2;
+				$line = <BLAST>;
+				my @data = split (" ", $line);
+				$evalue = $data[-1];
+				$line = <BLAST>;
+				if ($line =~ /in\s+(\d+)\s+aa\s+overlap/) {
+					$overlap = $1;
+				}
+				else {
+					$overlap = "0";
+				}
+			}
+			next if ($tename); # exit the loop once a hit has been found
 		}
-		print "\n";
+	}
+
+	# return the results
+	if ($tename) {
+		my $percent_overlap = int(($overlap/$te_length) * 100);
+		return ($tename, $evalue, $percent_overlap); 
+	}
+	else {
+		return ("no_match", "N/A", "N/A");
 	}
 }
 
 sub printUsage{
 
 print STDOUT "DESCRIPTION: This program takes bed formated files from RepeatMasker and from genome TIR analysis and returns report\n";
-print STDOUT "USAGE : combine-tir -r \"RepeatMasker .bed file\" -t \"TIR analysis .bed file\" -g \"Genome fasta file\" -o \"output file\"
+print STDOUT "USAGE : combine-tir -r \"RepeatMasker .bed file\" -t \"TIR analysis .bed file\" -g \"Genome fasta file\" -d \"datbase of TE proteins\" -o \"output file\"
 Options : 
     -r | rmbed		RepeatMasker output from RepeatExplorer file, bed formated (Mandatory)
     -t | tirbed		Whole genome TIR analysis, bed formated (Mandatory)
     -g | genome		Fasta formated genome (Mandatory)
+    -d | dbte		Database of TE proteins (default $config{dbte})
     -o | out   		Name of ouput file (default \"out\")\n";
     exit;
 }
